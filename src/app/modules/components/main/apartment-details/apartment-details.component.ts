ae3aee0dbce7b9f21eb392bf060b8b9d7a5cb05b
Alessandro <alevivoli81@gmail.com>
2022-07-22 12:47:45 +0200

    Added essentials

---
+++ src/app/modules/components/main/apartment-details/apartment-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apartment-details',
+  templateUrl: './apartment-details.component.html',
+  styleUrls: ['./apartment-details.component.scss']
+})
+export class ApartmentDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
