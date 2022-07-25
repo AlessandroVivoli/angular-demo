49c1d9a458328fdaca99881153c66f968c58a796
Alessandro Vivoli <alevivoli85@gmail.com>
2022-07-25 20:20:51 +0200

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { FavoritesComponent } from './modules/components/favorites/favorites.component';
+import { LoginComponent } from './modules/components/login/login.component';
+import { MainComponent } from './modules/components/main/main.component';
+import { RecommendationsComponent } from './modules/components/recommendations/recommendations.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'favorites', component: FavoritesComponent }
+  { path: '', component: MainComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'recommendations', component: RecommendationsComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
