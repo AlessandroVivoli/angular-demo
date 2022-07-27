d157e11228e68ec30dbc6c5b16b6216338a72674
Alessandro Vivoli <alevivoli85@gmail.com>
2022-07-27 17:03:36 +0200

    Added locations routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FavoritesComponent } from './modules/components/favorites/favorites.component';
+import { LocationsComponent } from './modules/components/locations/locations.component';
 import { LoginComponent } from './modules/components/login/login.component';
 import { MainComponent } from './modules/components/main/main.component';
 import { RecommendationsComponent } from './modules/components/recommendations/recommendations.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
+  { path: 'locations', component: LocationsComponent },
   { path: 'favorites', component: FavoritesComponent },
   { path: 'recommendations', component: RecommendationsComponent },
   { path: 'login', component: LoginComponent }
