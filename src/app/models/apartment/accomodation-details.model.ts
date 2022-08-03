1253138d3ca5c37c7b84a35fb38e36d4f4211dac
Alessandro <alevivoli81@gmail.com>
2022-08-03 09:28:02 +0200

    Removed unnecessary DTO

--- src/app/models/apartment/accomodation-details.model.ts
+++
@@ -1,8 +0,0 @@
-export interface AccomodationDetailsModel {
-    summary: string;
-    numberOfGuests: number;
-    cancellationFee: number;
-    accomodationType: string;
-    postalCode: number;
-    img: string;
-}
