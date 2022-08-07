97d72de789c986b316e66ab9079f0f97b5a93822
Alessandro Vivoli <alevivoli85@gmail.com>
2022-08-07 11:37:28 +0200

    Refactored models to generate uuid

--- src/app/models/location.model.ts
+++ src/app/models/location.model.ts
@@ -1,6 +1,12 @@
-export interface LocationModel {
-    id: string;
-    name: string;
-    postalCode: number;
-    imageUrl: string;
+import {v4 as uuid} from 'uuid';
+
+export class LocationModel {
+    id!: string;
+    name!: string;
+    postalCode!: number;
+    imageUrl!: string;
+
+    constructor() {
+        this.id = uuid();
+    }
 }
--- src/app/models/reservation.model.ts
+++ src/app/models/reservation.model.ts
@@ -1,9 +1,15 @@
-export interface ReservationModel {
-    id: string;
-    email: string;
-    accommodationId: string;
-    checkIn: string;
-    checkOut: string;
-    personsCount: number;
-    confirmed: boolean;
+import {v4 as uuid} from 'uuid';
+
+export class ReservationModel {
+    id!: string;
+    email!: string;
+    accommodationId!: string;
+    checkIn!: string;
+    checkOut!: string;
+    personsCount!: number;
+    confirmed!: boolean;
+
+    constructor() {
+        this.id = uuid();
+    }
 }
