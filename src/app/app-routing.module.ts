882df2ab3f14497e3c3df4009af7550e9d19830e
Alessandro Vivoli <alevivoli85@gmail.com>
2022-08-08 11:42:21 +0200

    Changed routing /home to home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { AccommodationBookingComponent } from './modules/components/accommodations/accommodation-booking/accommodation-booking.component';
 import { AccommodationDetailsComponent } from './modules/components/accommodations/accommodation-details/accomodation-details.component';
 import { AccommodationComponent } from './modules/components/accommodations/accommodation.component';
 import { FavoritesComponent } from './modules/components/favorites/favorites.component';
 import { LocationsComponent } from './modules/components/locations/locations.component';
 import { LoginComponent } from './modules/components/login/login.component';
-import { MainComponent } from './modules/components/main/main.component';
+import { HomeComponent } from './modules/components/home/home.component';
 import { MyBookingComponent } from './modules/components/my-booking/my-booking.component';
 
 const routes: Routes = [
-  { path: '', component: MainComponent },
+  { path: '', component: AppComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'locations', component: LocationsComponent },
   { path: 'accommodations', component: AccommodationComponent },
   { path: 'accommodations/:id', component: AccommodationDetailsComponent },
-  { path: 'accommodations/:id/book', component: AccommodationBookingComponent},
+  { path: 'accommodations/:id/book', component: AccommodationBookingComponent },
   { path: 'favorites', component: FavoritesComponent },
   { path: 'bookings', component: MyBookingComponent },
   { path: 'login', component: LoginComponent }
