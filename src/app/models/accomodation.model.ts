f68b67f8be8467753aba257dcc601c9ae6fc86b1
Alessandro Vivoli <alevivoli85@gmail.com>
2022-08-15 15:22:26 +0200

    Updated accomodation DTO

--- src/app/models/accomodation.model.ts
+++ src/app/models/accomodation.model.ts
@@ -1,24 +1,19 @@
 import { AccomodationTypeEnum } from "src/app/enums/accomodation-type.enum";
-import {v4 as uuid} from 'uuid';
 import { LocationModel } from "./location.model";
 
 export class AccomodationModel {
     id!: string;
-    title!: string;
+    title?: string;
     subtitle?: string;
     description?: string;
     shortDescription?: string;
     type!: AccomodationTypeEnum;
-    categorization!: number;
+    categorization?: number;
     personCount?: number;
     imageUrl?: string;
     freeCancelation!: boolean;
     price!: number;
     location!: LocationModel;
-    locationID!: string;
-    capacity!: number;
-
-    constructor() {
-        this.id = uuid();
-    }
+    locationID?: string;
+    capacity?: number;
 }
