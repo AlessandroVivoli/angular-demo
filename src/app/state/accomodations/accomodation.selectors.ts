b63e1570134a8ddc010b2d0ca39da69a6736c45c
Alessandro Vivoli <alevivoli85@gmail.com>
2022-08-15 13:46:08 +0200

    Added accomodation NgRx Selectors

---
+++ src/app/state/accomodations/accomodation.selectors.ts
@@ -0,0 +1,9 @@
+import { createSelector } from '@ngrx/store';
+import { AppState } from '../app.state';
+import { AccomodationState } from './accomodation.reducer';
+
+export const selectAccomodations = (state: AppState) => state.accomodations;
+export const selectAllAccomodations = createSelector(selectAccomodations, (state: AccomodationState) => state.accomodations);
+export const selectAccomodationError = createSelector(selectAccomodations, (state: AccomodationState) => state.error);
+export const selectAccomodation = createSelector(selectAccomodations, (state: AccomodationState) => state.accomodation);
+export const selectLoading = createSelector(selectAccomodations, (state: AccomodationState) => state.loading);
