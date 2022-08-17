cc82b1d4fdec3c396302d024bc7f598e5810689e
Alessandro <alevivoli81@gmail.com>
2022-08-17 17:00:29 +0200

    Added adding places possible

--- src/app/modules/components/my-accomodation-form/my-accomodation-form.component.ts
+++ src/app/modules/components/my-accomodation-form/my-accomodation-form.component.ts
@@ -1,47 +1,105 @@
-import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, Subscription } from 'rxjs';
 import { AccomodationTypeEnum } from 'src/app/enums/accomodation-type.enum';
+import { AccomodationModel } from 'src/app/models/accomodation.model';
+import { CustomErrorResponse } from 'src/app/models/custom-error-response.model';
+import { PostAccomodation } from 'src/app/state/accomodations/accomodation.actions';
+import { selectAccomodationError, selectAccomodationLoading } from 'src/app/state/accomodations/accomodation.selectors';
+import { AppState } from 'src/app/state/app.state';
+
+import * as uuid from 'uuid';
 
 @Component({
-  selector: 'app-my-accomodation-form',
-  templateUrl: './my-accomodation-form.component.html',
-  styleUrls: ['./my-accomodation-form.component.scss']
+	selector: 'app-my-accomodation-form',
+	templateUrl: './my-accomodation-form.component.html',
+	styleUrls: ['./my-accomodation-form.component.scss']
 })
-export class MyAccomodationFormComponent implements OnInit, AfterViewInit {
-  name = new FormControl();
-  shortDesc = new FormControl();
-  longDesc = new FormControl();
-  categorization = new FormControl();
-  type = new FormControl();
-  price = new FormControl();
-  location = new FormControl();
-  postalCode = new FormControl();
-  freeCancellation = new FormControl<boolean>(false);
+export class MyAccomodationFormComponent implements OnInit, AfterViewInit, OnDestroy {
+	name = new FormControl();
+	shortDesc = new FormControl();
+	longDesc = new FormControl();
+	categorization = new FormControl();
+	type = new FormControl();
+	price = new FormControl();
+	location = new FormControl();
+	postalCode = new FormControl();
+	freeCancellation = new FormControl<boolean>(false);
 
-  @ViewChild('stars')
-  starContainer: ElementRef<HTMLDivElement>;
+	@ViewChild('stars')
+	starContainer: ElementRef<HTMLDivElement>;
 
-  data: { inputValue: string, label: string }[] = Object.keys(AccomodationTypeEnum).filter(item => {
-    return isNaN(Number(item));
-  }).map(item => { return { inputValue: item, label: item } });
+	data: { inputValue: string; label: string }[] = Object.keys(AccomodationTypeEnum)
+		.filter((item) => {
+			return isNaN(Number(item));
+		})
+		.map((item) => {
+			return { inputValue: item, label: item };
+		});
 
-  constructor() { }
+	accomodationLoading$: Observable<boolean>;
+	accomodationError$: Observable<CustomErrorResponse | undefined>;
 
-  ngOnInit(): void {
-  }
+	#sub: Subscription = new Subscription();
 
-  ngAfterViewInit(): void {
-    const children = this.starContainer.nativeElement.children;
+	constructor(private store: Store<AppState>, private toast: ToastrService) {
+		this.accomodationLoading$ = this.store.select(selectAccomodationLoading);
+		this.accomodationError$ = this.store.select(selectAccomodationError);
+	}
 
-    for (let i = 0; i < children.length; i++)
-      (children.item(i) as HTMLButtonElement)!.addEventListener('click', () => {
-        this.categorization.setValue(children.length - i);
-        for (let i = 0; i < children.length; i++) {
-          if (i < this.categorization.value)
-            children.item(children.length - 1 - i)?.classList.add('selected');
-          else children.item(children.length - 1 - i)?.classList.remove('selected');
-        }
-      });
-  }
+	ngOnInit(): void {}
 
+	ngAfterViewInit(): void {
+		const children = this.starContainer.nativeElement.children;
+
+		for (let i = 0; i < children.length; i++)
+			(children.item(i) as HTMLButtonElement)!.addEventListener('click', () => {
+				this.categorization.setValue(children.length - i);
+				for (let i = 0; i < children.length; i++) {
+					if (i < this.categorization.value) children.item(children.length - 1 - i)?.classList.add('selected');
+					else children.item(children.length - 1 - i)?.classList.remove('selected');
+				}
+			});
+	}
+
+	ngOnDestroy(): void {}
+
+	onSubmit() {
+		const accomodation = new AccomodationModel();
+
+		accomodation.id = uuid.v4();
+		accomodation.title = this.name.value;
+		accomodation.description = this.longDesc.value;
+		accomodation.shortDescription = this.shortDesc.value;
+		accomodation.type = this.type.value;
+		accomodation.categorization = this.categorization.value;
+		accomodation.freeCancelation = !!this.freeCancellation.value;
+		accomodation.price = this.price.value;
+		accomodation.location = { id: uuid.v4(), name: this.location.value, postalCode: this.postalCode.value, properties: 1, imageUrl: '' };
+		accomodation.locationID = accomodation.location.id;
+
+		const favorites = JSON.parse(localStorage.getItem('favorites') as string) as string[];
+
+		if (favorites) {
+			favorites.push(accomodation.id);
+			localStorage.setItem('favorites', JSON.stringify(favorites));
+		} else {
+      localStorage.setItem('favorites', JSON.stringify([accomodation.id]));
+		}
+
+		this.store.dispatch(PostAccomodation({ payload: accomodation }));
+
+		this.#sub.add(
+			this.accomodationLoading$.subscribe((loading) => {
+				if (!loading) {
+					this.accomodationError$.subscribe((error) => {
+						if (error) this.toast.error(error.statusText);
+						else this.toast.success('Successfully added place');
+					});
+				}
+			})
+		);
+	}
 }
--- src/app/modules/components/my-places/my-places.component.ts
+++ src/app/modules/components/my-places/my-places.component.ts
@@ -1,18 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
 import { AccomodationModel } from 'src/app/models/accomodation.model';
+import { CustomErrorResponse } from 'src/app/models/custom-error-response.model';
+import { GetAccomodations } from 'src/app/state/accomodations/accomodation.actions';
+import { selectAccomodationError, selectAccomodationLoading, selectAllAccomodations } from 'src/app/state/accomodations/accomodation.selectors';
+import { AppState } from 'src/app/state/app.state';
 
 @Component({
-  selector: 'app-favorites',
-  templateUrl: './my-places.component.html',
-  styleUrls: ['./my-places.component.scss']
+	selector: 'app-favorites',
+	templateUrl: './my-places.component.html',
+	styleUrls: ['./my-places.component.scss']
 })
-export class MyPlacesComponent implements OnInit {
+export class MyPlacesComponent implements OnInit, OnDestroy {
+	accomodations$: Observable<AccomodationModel[]>;
+	accomodationsLoading$: Observable<boolean>;
+	accomodationsError$: Observable<CustomErrorResponse | undefined>;
 
-  accomodations: AccomodationModel[] = [];
+	favoriteAccomodations: AccomodationModel[] = [];
 
-  constructor() { }
+	#sub: Subscription = new Subscription();
 
-  ngOnInit(): void {
-  }
+	constructor(private store: Store<AppState>) {
+		this.accomodations$ = this.store.select(selectAllAccomodations);
+		this.accomodationsLoading$ = this.store.select(selectAccomodationLoading);
+		this.accomodationsError$ = this.store.select(selectAccomodationError);
+	}
 
+	ngOnInit(): void {
+		this.store.dispatch(GetAccomodations());
+
+		this.#sub.add(
+			this.accomodations$.subscribe((accomodations) => {
+				const favorites = localStorage.getItem('favorites');
+				if (favorites)
+					this.favoriteAccomodations = accomodations.filter((accomodation) => (JSON.parse(favorites) as string[]).includes(accomodation.id));
+			})
+		);
+	}
+
+	ngOnDestroy(): void {
+		this.#sub.unsubscribe();
+	}
 }
