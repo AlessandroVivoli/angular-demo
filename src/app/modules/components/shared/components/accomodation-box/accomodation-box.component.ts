b8552887ed126e7ef06d92ef138a6e73638de797
Alessandro <alevivoli81@gmail.com>
2022-08-17 14:08:51 +0200

    Fixed the issue with location not loading

--- src/app/modules/components/shared/components/accomodation-box/accomodation-box.component.ts
+++ src/app/modules/components/shared/components/accomodation-box/accomodation-box.component.ts
@@ -1,18 +1,20 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { filter, Observable, of, Subscription } from 'rxjs';
 import { AccomodationModel } from 'src/app/models/accomodation.model';
 import { LocationModel } from 'src/app/models/location.model';
 import { AppState } from 'src/app/state/app.state';
 import { GetLocation } from 'src/app/state/location/location.actions';
 import { selectOneLocation } from 'src/app/state/location/location.selectors';
+import { GetLocations } from 'src/app/state/locations/locations.actions';
+import { selectAllLocations, selectLocations } from 'src/app/state/locations/locations.selectors';
 
 @Component({
 	selector: 'app-accomodation-box',
 	templateUrl: './accomodation-box.component.html',
 	styleUrls: ['./accomodation-box.component.scss']
 })
-export class AccomodationBoxComponent implements OnInit {
+export class AccomodationBoxComponent implements OnInit, OnDestroy {
 	@Input() accomodation: AccomodationModel;
 	@Input() changeLayout: boolean;
 	@Input() href?: string = undefined;
@@ -26,14 +28,30 @@ export class AccomodationBoxComponent implements OnInit {
 	@Output('OnClick')
 	accomodationOutput: EventEmitter<AccomodationModel> = new EventEmitter();
 
-	location$: Observable<LocationModel | undefined>;
+	locations$: Observable<LocationModel[]>;
+	location$: Observable<LocationModel>;
+
+	#sub: Subscription = new Subscription();
 
 	constructor(private store: Store<AppState>) {
-		this.location$ = this.store.select(selectOneLocation);
+		this.locations$ = this.store.select(selectAllLocations);
 	}
 
 	ngOnInit(): void {
-		if (!this.accomodation.location && this.accomodation.locationID) this.store.dispatch(GetLocation({ payload: this.accomodation.locationID }));
+		if (!this.accomodation.location) {
+			this.store.dispatch(GetLocations());
+			this.#sub.add(
+				this.locations$.subscribe((locations) => {
+					this.location$ = of(locations.find((location) => location.id === this.accomodation.locationID) as LocationModel);
+				})
+			);
+		}
+
+		if (!this.accomodation.location && !!this.accomodation.locationID) this.store.dispatch(GetLocation({ payload: this.accomodation.locationID }));
+	}
+
+	ngOnDestroy(): void {
+		this.#sub.unsubscribe;
 	}
 
 	onClick() {
