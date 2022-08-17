d36434b64eded364e0f066b675b294fa5e60a53c
Alessandro <alevivoli81@gmail.com>
2022-08-17 14:08:15 +0200

    Added further logging for debugging purposes

--- src/app/services/logging-interceptor.service.ts
+++ src/app/services/logging-interceptor.service.ts
@@ -1,13 +1,18 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of, pipe, tap, throwError } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 
 @Injectable()
 export class LoggingInterceptorService implements HttpInterceptor {
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		return next.handle(req).pipe(catchError((error) => {
-            console.log(error);
-            return throwError(() => error);
-        }));
+		return next.handle(req).pipe(
+			tap((next) => {
+				if (next.type === HttpEventType.Response) console.log(next);
+			}),
+			catchError((error) => {
+				console.log(error);
+				return throwError(() => error);
+			})
+		);
 	}
 }
