71c8ea5be471066411cd540a1d1e117186eed74f
Alessandro Vivoli <alevivoli85@gmail.com>
2022-08-19 15:33:00 +0200

    Formatted the code

--- src/app/modules/components/login/login.component.ts
+++ src/app/modules/components/login/login.component.ts
@@ -1,39 +1,35 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+	@ViewChild('loginForm') el: ElementRef<HTMLFormElement>;
 
-  @ViewChild('loginForm') el: ElementRef<HTMLFormElement>;
+	email: string = '';
 
-  email: string = '';
+	constructor(private authService: AuthService, private router: Router) {
+		if (authService.isLoggedIn()) router.navigate(['']);
+	}
 
-  constructor(private authService: AuthService, private router: Router) {
-    if(authService.isLoggedIn())
-      router.navigate(['']);
-  }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+	submit(event: SubmitEvent) {
+		if (!this.el.nativeElement.checkValidity()) {
+			event.preventDefault();
+			event.stopPropagation();
+		} else {
+			this.authService.login();
 
-  submit(event: SubmitEvent) {
-    if(!this.el.nativeElement.checkValidity()) {
-      event.preventDefault();
-      event.stopPropagation();
-    }else {
-      this.authService.login();
+			localStorage.setItem('email', this.email);
 
-      localStorage.setItem('email', this.email);
+			location.reload();
+		}
 
-      location.reload();
-    }
-
-    this.el.nativeElement.classList.add('was-validated');
-  }
-
+		this.el.nativeElement.classList.add('was-validated');
+	}
 }
